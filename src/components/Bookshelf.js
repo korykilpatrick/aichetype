aea4729b12fd066de60489363e95447cc3dc0035
Kory Kilpatrick <koryrkilpatrick@gmail.com>
2023-09-29 14:29:30 -0500

    stylized bookshelf w/ css

--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -3,6 +3,7 @@ import '../css/Bookshelf.css';
 
 const Bookshelf = () => {
   const [books, setBooks] = useState([]);
+  const booksPerRow = 5 // Adjust this number based on how many books you want in a row
 
   useEffect(() => {
     fetch('/api/books')
@@ -20,19 +21,32 @@ const Bookshelf = () => {
     return stars;
   };
 
-  return (
-    <div className="bookshelf">
-      {books.map((book) => (
+  const renderRow = (rowBooks) => {
+    return rowBooks.map((book) => (
         <div key={book.id} className="book-item">
-          <a href={book.book_link} target="_blank"><img src={book.img_url} alt={book.title} className="book-image" /></a>
+          <a href={book.book_link} target="_blank" rel="noopener noreferrer"><img src={book.img_url} alt={book.title} className="book-image" /></a>
           <div className="book-info">
-            <p>Author: <a href={book.author_link} target="_blank">{book.author}</a></p>
-            <p>Date read: {book.date_read ? book.date_read : book.date_started}</p>
             <p>Rating: {book.rating ? renderStars(book.rating) : 'N/A'}</p>
-            <p>Date Published: {book.date_pub}</p>
+            <p>Date read: {book.date_read ? book.date_read : book.date_started}</p>
+            {/* <p>Date Published: {book.date_pub}</p> */}
           </div>
         </div>
-      ))}
+      ))
+    };
+
+  return (
+    <div className="bookshelf" styles={{ backgroundImage: `url('/images/wood-texture.jpg')`}}>
+      {Array.from({ length: Math.ceil(books.length / booksPerRow) }, (_, i) => {
+        const start = i * booksPerRow;
+        const end = start + booksPerRow;
+        const rowBooks = books.slice(start, end);
+        return (
+          <div key={i} className="book-row">
+            {renderRow(rowBooks)}
+            <div className="shelf"></div>
+          </div>
+        );
+      })}
     </div>
   );
 };
